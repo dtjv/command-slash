0bef76b2f9d63db206e1be8f0b4adfc708fd7f3c
David Valles <davidtjvalles@gmail.com>
2021-11-03 05:44:00 -0700

    Execute selected command via keyboard

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,82 @@
 import { useState } from 'react'
 
-const DEFAULT_ITEMS = [{ id: 'A' }, { id: 'B' }, { id: 'C' }]
+const DEFAULT_ITEMS = [
+  { id: 'A', display: 'Insert 🐕' },
+  { id: 'B', display: 'Say Hi' },
+  { id: 'C', display: 'Delete Text' },
+]
 
 export const App = () => {
   const [text, setText] = useState('')
   const [items, setItems] = useState(DEFAULT_ITEMS)
+  const [showMenu, setShowMenu] = useState(false)
   const [selectedId, setSelectedId] = useState('A')
 
+  const insertDog = () => {
+    setText((text) => `${text} 🐕`)
+  }
+
+  const deleteText = () => {
+    setText('')
+  }
+
+  const sayHi = () => {
+    alert('hi!')
+  }
+
+  const commands = [insertDog, sayHi, deleteText]
+
+  const handleChangeSelected = (event) => {
+    event.preventDefault()
+
+    const selectedIdx = items.findIndex((item) => item.id === selectedId)
+    const offset = event.key === 'ArrowDown' ? 1 : -1
+
+    if (selectedIdx !== -1) {
+      let nextIdx = selectedIdx + offset
+      nextIdx =
+        nextIdx < 0 ? 0 : nextIdx === items.length ? nextIdx - 1 : nextIdx
+
+      setSelectedId(items[nextIdx].id)
+    }
+  }
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+      case 'ArrowDown':
+        if (showMenu) {
+          handleChangeSelected(event)
+        }
+        break
+      case 'Enter':
+        if (showMenu) {
+          const selectedIdx = items.findIndex((item) => item.id === selectedId)
+          if (selectedIdx !== -1) {
+            setText((text) => text.substring(0, text.length - 1))
+            commands[selectedIdx]()
+            setShowMenu(false)
+          }
+        }
+        break
+      case '/':
+        setShowMenu(true)
+      default:
+      // noop
+    }
+  }
+
   const handleChange = (e) => {
-    const v = e.currentTarget.value
-    setText(v)
+    const inputValue = e.currentTarget.value
+    setText(inputValue)
+
+    if (inputValue === '') {
+      setSelectedId(0)
+    }
   }
 
   const handleMouseOver = (id) => {
     setSelectedId(id)
-  }
-
-  const handleKeyDown = (e) => {
-    const selectedIdx = items.findIndex((item) => item.id === selectedId)
-    const offset = e.key === 'ArrowDown' ? 1 : e.key === 'ArrowUp' ? -1 : 0
-
-    if (offset !== 0) {
-      e.preventDefault()
-
-      let nextIdx = selectedIdx + offset
-      nextIdx =
-        nextIdx < 0 ? 0 : nextIdx === items.length ? nextIdx - 1 : nextIdx
-      setSelectedId(items[nextIdx].id)
-    }
   }
 
   return (
@@ -43,37 +92,39 @@ export const App = () => {
             onKeyDown={handleKeyDown}
             style={{ padding: '.5rem', border: '1px solid black' }}
           />
-          <div
-            style={{
-              marginTop: '1rem',
-              padding: '.5rem',
-            }}
-          >
-            <ul role="listbox" aria-activedescendant={selectedId}>
-              {items.map((item) => (
-                <li
-                  key={item.id}
-                  id={item.id}
-                  role="option"
-                  aria-selected={selectedId === item.id}
-                  style={{
-                    ...{
-                      cursor: 'pointer',
-                      margin: '.5rem',
-                      padding: '.5rem',
-                      backgroundColor: '#eee',
-                    },
-                    ...(selectedId === item.id
-                      ? { backgroundColor: 'lightblue' }
-                      : { backgroundColor: '#eee' }),
-                  }}
-                  onMouseOver={() => handleMouseOver(item.id)}
-                >
-                  {item.id}
-                </li>
-              ))}
-            </ul>
-          </div>
+          {showMenu && (
+            <div
+              style={{
+                marginTop: '1rem',
+                padding: '.5rem',
+              }}
+            >
+              <ul role="listbox" aria-activedescendant={selectedId}>
+                {items.map((item) => (
+                  <li
+                    key={item.id}
+                    id={item.id}
+                    role="option"
+                    aria-selected={selectedId === item.id}
+                    style={{
+                      ...{
+                        cursor: 'pointer',
+                        margin: '.5rem',
+                        padding: '.5rem',
+                        backgroundColor: '#eee',
+                      },
+                      ...(selectedId === item.id
+                        ? { backgroundColor: 'lightblue' }
+                        : { backgroundColor: '#eee' }),
+                    }}
+                    onMouseOver={() => handleMouseOver(item.id)}
+                  >
+                    {item.display}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
